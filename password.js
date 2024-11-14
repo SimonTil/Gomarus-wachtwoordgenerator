fd8182fa6ecdf6bfcee5384b8d31b13fd8900edc
SimonT <simontillema@gmail.com>
2024-11-14 13:36:07 +0100

    Add onload and other functionalities
    Add onload functionality, prevent start pattern by shuffling entire password, fix bug on generation of short passwords, and some code simplification.

--- password.js
+++ password.js
@@ -1,24 +1,18 @@
 function verifyInput(){
-    if (!document.forms[0].elements[0].checked &&
-        !document.forms[0].elements[1].checked &&
-        !document.forms[0].elements[2].checked &&
-        !document.forms[0].elements[3].checked){
+    const form = document.forms[0];
+    if (![0, 1, 2, 3].some(i => form.elements[i].checked)) {
         alert('Vergeten aan te geven welke tekens in het wachtwoord moeten!');
         return 0;
     }
-    if (document.forms[0].elements[4].value == ""){
+    if (form.elements[4].value == ""){
         alert('Vergeten aantal karakters in te voeren');
         return 0;
     }
-    if (isNaN(document.forms[0].elements[4].value)){
+    if (isNaN(form.elements[4].value) || form.elements[4].value <= 0){
         alert('Foutieve invoer voor aantal karakters');
         return 0;
     }
     return 1;
-}
-
-function getRandomNum(lbound, ubound){
-    return Math.floor(Math.random() * (ubound - lbound)) + lbound;
 }
 
 function getRandomChar(upper, lower, number, symbols){
@@ -27,13 +21,13 @@ function getRandomChar(upper, lower, number, symbols){
     var numberChars = "23456789";
     var symbolChars = "!@#%?&/";
     var charSet = "";
-    
-    if (upper == true) charSet += upperChars;
-    if (lower == true) charSet += lowerChars;
-    if (number == true) charSet += numberChars;
-    if (symbols == true) charSet += symbolChars;
 
-    return charSet.charAt(getRandomNum(0, charSet.length));
+    if (upper) charSet += upperChars;
+    if (lower) charSet += lowerChars;
+    if (number) charSet += numberChars;
+    if (symbols) charSet += symbolChars;
+
+    return charSet.charAt(Math.floor(Math.random() * charSet.length));
 }
 
 function generatePassword(length, upper, lower, number, symbols){
@@ -61,9 +55,18 @@ function generatePassword(length, upper, lower, number, symbols){
         }
     }
 
+    var arr = password.split('');
+    arr.sort(() => Math.random() - 0.5);
+    password = arr.join('');
+
+    if (length < 4) {
+        password = password.substring(0, length);
+    }
+
     resetCopyButton();
     return password;
 }
+
 
 function copyToClipboard(){
     var text = document.forms[0].elements[6].value;
@@ -91,4 +94,8 @@ function has3IdenticalChars(password) {
         }
     }
     return false;
+}
+
+window.onload = function(){
+    document.forms[0].elements[6].value = generatePassword(12, true, true, true, true);
 }
